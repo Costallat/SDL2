479de54607d49ce1c12e3d5d961c92280ed8b200
Tom Mason <wheybags@wheybags.com>
2019-07-22 08:02:08 +0200

    disable metal backend for now
    Metal backend seems to be unimplemented in upstream cmake. I presume it
    works if you use their xcode project instead.

--- include/SDL_config_macosx.h
+++ include/SDL_config_macosx.h
@@ -203,18 +203,11 @@
 #endif
 
 /* Metal only supported on 64-bit architectures with 10.11+ */
-#if TARGET_RT_64_BIT && (MAC_OS_X_VERSION_MAX_ALLOWED >= 101100)
-#define SDL_PLATFORM_SUPPORTS_METAL    1
-#else
-#define SDL_PLATFORM_SUPPORTS_METAL    0
-#endif
-
-#ifndef SDL_VIDEO_RENDER_METAL
-#if SDL_PLATFORM_SUPPORTS_METAL
-#define SDL_VIDEO_RENDER_METAL    1
-#else
+//#if TARGET_CPU_X86_64 && (MAC_OS_X_VERSION_MAX_ALLOWED >= 101100)
+//#define SDL_VIDEO_RENDER_METAL    1
+//#else
 #define SDL_VIDEO_RENDER_METAL    0
-#endif
+//#endif
 #endif
 
 /* Enable OpenGL support */
